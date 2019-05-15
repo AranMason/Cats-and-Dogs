652dcc1e173c1819ad5f28dd5850fc2d203eb68b
Aran Mason <a.mason@hpa.org.nz>
2019-05-15 14:15:40 +1200

    Added create model flow

---
+++ flask/create_model.py
@@ -0,0 +1,46 @@
+import numpy as np
+import keras
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers import Activation
+from keras.layers.core import Dense, Flatten
+from keras.optimizers import Adam
+from keras.metrics import categorical_crossentropy
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import *
+# from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix
+import itertools
+# import matplotlib.pyplot as ply
+# %matplotlib inline
+
+file_classes = ['dogs', 'cats']
+
+train_path = 'data/train'
+valid_path = 'data/valid'
+test_path = 'data/test'
+
+train_batches = ImageDataGenerator().flow_from_directory(train_path, target_size=(224,224), classes=file_classes, batch_size=10)
+valid_batches = ImageDataGenerator().flow_from_directory(valid_path, target_size=(224, 224), classes=file_classes, batch_size=4)
+test_batches = ImageDataGenerator().flow_from_directory(test_path, target_size=(224, 224), classes=file_classes, batch_size=8)
+
+vgg16_model = keras.applications.vgg16.VGG16(include_top=False)
+
+
+#Convert to a Sequential Model Type
+model = Sequential()
+for layer in vgg16_model.layers:
+	layer.trainable = False
+	model.add(layer)
+
+model.add(Dense(len(file_classes), activation='softmax'))
+
+model.summary()
+
+model.compile(Adam(lr=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit_generator(train_batches, steps_per_epoch=4,
+					validation_data=valid_batches, validation_steps=4, epochs=10, verbose=2)
+
+model.save_weights('model.h5')
