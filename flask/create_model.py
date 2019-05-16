f8966a4c2f34ff55a5fecde030a971f9716f6d70
Aran Mason <a.mason@hpa.org.nz>
2019-05-16 15:04:35 +1200

    Now export a file with the classes for reference

--- flask/create_model.py
+++ flask/create_model.py
@@ -8,31 +8,32 @@ from keras.optimizers import Adam
 from keras.metrics import categorical_crossentropy
 from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
 from keras.layers.normalization import BatchNormalization
-# from keras.layers.convolutional import *
-# from matplotlib import pyplot as plt
+import json
 from sklearn.metrics import confusion_matrix
 import itertools
-# import matplotlib.pyplot as ply
-# %matplotlib inline
 
-file_classes = ['dogs', 'cats']
 
-epochs = 20
+
+epochs = 30
 
 train_path = 'data/train'
 valid_path = 'data/valid'
 test_path = 'data/test'
 
-train_batches = ImageDataGenerator().flow_from_directory(train_path, target_size=(224,224), classes=file_classes, batch_size=10)
-valid_batches = ImageDataGenerator().flow_from_directory(valid_path, target_size=(224, 224), classes=file_classes, batch_size=4)
+train_batches = ImageDataGenerator().flow_from_directory(train_path, target_size=(224,224),  batch_size=10)
+valid_batches = ImageDataGenerator().flow_from_directory(valid_path, target_size=(224, 224),  batch_size=4) #classes=file_classes,
 # test_batches = ImageDataGenerator().flow_from_directory(test_path, target_size=(224, 224), classes=file_classes, batch_size=8)
+
+print("Classes: ", train_batches.class_indices)
+
+file_classes = train_batches.class_indices
 
 imgs, labels = next(train_batches)
 
 vgg16_model = keras.applications.vgg16.VGG16()
 
 
-#Convert to a Sequential Model Type
+#Convert to a Sequential Model Type, remove the top layer
 model = Sequential()
 for layer in vgg16_model.layers[:-1]:
     model.add(layer)
@@ -41,7 +42,7 @@ for layer in model.layers:
     layer.trainable = False
 
 
-model.add(Dense(len(file_classes), activation='softmax'))
+model.add(Dense(len(file_classes), activation='softmax', name='predictions'))
 
 model.summary()
 
@@ -51,27 +52,18 @@ model.fit_generator(train_batches, steps_per_epoch=4,
 					validation_data=valid_batches, validation_steps=4, epochs=epochs, verbose=2)
 
 
-json = model.to_json()
+json_model = model.to_json()
 with open('models/model.json', 'w') as json_file:
-	json_file.write(json)
+	json_file.write(json_model)
 
 model.save_weights('models/model_weights.h5')
 
 model.save('models/model_complete.h5')
 print("Saved Model")
 
-# def preprocess_image(image, target_size):
-# 	if image.mode != 'RGB':
-# 		image = image.convert('RGB')
+# Saving Classes for reference later
 
-# 	image = image.resize(target_size)
-# 	image = img_to_array(image)
-# 	image = np.expand_dims(image, axis=0)
+with open('models/classes.json', 'w') as outfile:
+	print(type(file_classes))
+	json.dump(file_classes, outfile)
 
-# 	return image
-
-# img = preprocess_image(load_img('./test_img.jpg'), target_size=(224, 224))
-# print("Making Test Prediction")
-# predict = model.predict(img)
-
-# print(predict)
